4be2d130070777490bca8a5ddf8b9a72639bbec3
5555nika <niiarasisman@gmail.com>
2026-08-18 17:45:15 +0300

    fix: update loader interface and add input styles

--- src/components/UI/loader/MyLoader.tsx
+++ src/components/UI/loader/MyLoader.tsx
@@ -1,9 +1,13 @@
 import cl from './MyLoader.module.css'
+import {type  HTMLAttributes } from 'react'
 
-export const MyLoader = () => {
+export type ILoader = HTMLAttributes<HTMLDivElement> 
+
+
+export const MyLoader = ({ className,  ...props}: ILoader) => {
     return (
         <div className={cl['loading-container']}>
-            <div className={cl.loading}></div>
+            <div className={[cl.loading, className].filter(Boolean).join(' ')} {...props}></div>
         </div>
     )
 }
